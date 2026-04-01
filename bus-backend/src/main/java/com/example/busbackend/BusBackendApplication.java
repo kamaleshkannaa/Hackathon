324a8aec19509826cc95ef96d97d64fa5edff646
kamaleshkannaa <120234666+kamaleshkannaa@users.noreply.github.com>
2026-04-01 11:10:00 +0530

    Initial project setup - backend

---
+++ bus-backend/src/main/java/com/example/busbackend/BusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.busbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusBackendApplication.class, args);
+    }
+
+}
